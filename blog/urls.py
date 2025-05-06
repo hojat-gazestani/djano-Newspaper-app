0795aefe40c88d452d6fa4daee9acfce8c67ae77
hojat-gazestani <hojatgazestani@gmail.com>
2025-05-06 18:22:01 +0330

    Delete blog post with DeleteView

--- blog/urls.py
+++ blog/urls.py
@@ -4,11 +4,13 @@ from .views import (
     BlogDetailView, 
     BlogCreateView,
     BlogUpdateView,
+    BlogDeleteView,
 )
 
 urlpatterns = [
   path("blog/new/", BlogCreateView.as_view(), name="post_new"),
   path("blog/<int:pk>/", BlogDetailView.as_view(), name="post_detail"),
   path("blog/<int:pk>/edit/", BlogUpdateView.as_view(), name="post_edit"),
+  path("blog/<int:pk>/delete/", BlogDeleteView.as_view(), name="post_delete"),
   path("", BlogListView.as_view(), name="blog"),
 ]
