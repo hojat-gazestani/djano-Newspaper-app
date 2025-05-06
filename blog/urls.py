644f51a95f697b3b5c88c8e8f50b5c686b486a67
hojat-gazestani <hojatgazestani@gmail.com>
2025-05-06 18:08:45 +0330

    Edit a blog post using UpdateView

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views import BlogListView, BlogDetailView, BlogCreateView
+from .views import (
+    BlogListView, 
+    BlogDetailView, 
+    BlogCreateView,
+    BlogUpdateView,
+)
 
 urlpatterns = [
   path("blog/new/", BlogCreateView.as_view(), name="post_new"),
   path("blog/<int:pk>/", BlogDetailView.as_view(), name="post_detail"),
+  path("blog/<int:pk>/edit/", BlogUpdateView.as_view(), name="post_edit"),
   path("", BlogListView.as_view(), name="blog"),
 ]
