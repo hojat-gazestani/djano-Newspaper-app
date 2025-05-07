89d62abf1a43365bbc4afed47191f4d738596c7f
hojat-gazestani <hojatgazestani@gmail.com>
2025-05-08 01:54:54 +0330

    Add user log in and log out

--- django_project/settings.py
+++ django_project/settings.py
@@ -27,7 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -124,3 +123,5 @@ STATICFILES_DIRS = [ BASE_DIR / "static"]
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL = "blog"
+LOGOUT_REDIRECT_URL = "blog"
--- django_project/urls.py
+++ django_project/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include("django.contrib.auth.urls")),
     path('pages/', include('pages.urls')),
     path('posts/', include('posts.urls')),
     path('blog/', include('blog.urls')),
